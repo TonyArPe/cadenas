add497e710f853d175589492bf20e5753109ad22
TonyArPe <tonyaragonp@gmail.com>
2023-01-12 08:56:44 +0100

    trabajo de modificacion de cadenas

--- src/ejercicios/Ejercicio1.java
+++ src/ejercicios/Ejercicio1.java
@@ -1,5 +1,5 @@
 package ejercicios;
-import java.util.Arrays;
+
 
 
 public class Ejercicio1 {
@@ -19,7 +19,7 @@ public class Ejercicio1 {
     //Un metodo que se le pase el String anterior de apellidos y nombre, nos diga
     //cuantas vocales tiene entre apellidos y nombre
     public static void main(String[] args) {
-        String[] datos = {"Antonio"," ", "Aragón"," ", "Calle Larios"," ", "56434783"};
+        String[] datos = {"Antonio" , "Aragon" , "calle Larios" , "56434783"};
         for (String dato: datos) {
             System.out.printf("%s%n", dato);
         }
@@ -27,24 +27,59 @@ public class Ejercicio1 {
         System.out.printf("CSV: %S%n", csv1);
 
         String csv2 = crearCampoCSVSinEspaciosYMayuculas(csv1);
+        System.out.printf("CSV: %S%n", csv2);
+
+        String csv3 = crearApellidosNombreMinuscula(csv2);
+        System.out.printf("CSV: %s%n", csv3);
+
+        int numeroVocalesNombre = contarVocalesDeNombre(csv1.toLowerCase());
+        System.out.printf("%s tiene %d vocales%n", csv1, numeroVocalesNombre);
     }
 
-    private static String crearCampoCSVSinEspaciosYMayuculas(String csv1) {
-        String csv = "";
-        for (String dato: datos) {
+
+    private static int contarVocalesDeNombre(String nombre) {
+        int contadorVocales = 0;
+        for (int i = 0; i < nombre.length(); i++) {
+            //if (nombre.toLowerCase().charAt(i) == 'a' || nombre.toLowerCase().charAt(i) == 'e' || nombre.toLowerCase().charAt(i) == 'i' || nombre.toLowerCase().charAt(i) == 'o' || nombre.toLowerCase().charAt(i) == 'u');
+            switch (nombre.charAt(i)) {
+                case 'a':
+                    contadorVocales++;
+                    break;
+                case 'e':
+                    contadorVocales++;
+                    break;
+                case 'i':
+                    contadorVocales++;
+                    break;
+                case 'o':
+                    contadorVocales++;
+                    break;
+                case 'u':
+                    contadorVocales++;
+                    break;
+            }
+        }
+        return contadorVocales;
+    }
+        private static String crearApellidosNombreMinuscula (String csv2){
+            String[] csv3 = csv2.split(",");
+            return csv3[1].toLowerCase() + " " + csv3[0].toLowerCase();
+        }
+
+        private static String crearCampoCSVSinEspaciosYMayuculas (String csv1){
+            String csv2 = "";
+            return csv2 = csv1.replaceAll(", ", ",").toUpperCase();
 
         }
-        return null;
-    }
 
-    private static String crearCampoCSV(String[] datos) {
-        String csv = "";
-        for (String dato: datos) {
-            csv += dato +", ";
+        private static String crearCampoCSV (String[]datos){
+            String csv = "";
+            for (String dato : datos) {
+                csv += dato + ", ";
+            }
+            if (csv.isEmpty())
+                return csv;
+            return csv.substring(0, csv.length() - 2);
         }
-        if (csv.isEmpty())
-            return csv;
-        return csv.substring(0, csv.length()-2);
-    }
 
-}
+    }
